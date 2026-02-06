435cf3cb133e69831e12a70238d414f2c5d9ef2f
Admin <rp3truhin@yandex.ru>
2026-02-06 15:24:44 +0400

    init commit

---
+++ src/main/java/ru/petrukhin_magera/matrix_calculator/MatrixCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.petrukhin_magera.matrix_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/petrukhin_magera/matrix_calculator/MatrixCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrukhin_magera.matrix_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
